3771778dc9fd6428716f4a64161be21130a31529
Eric Smith <ydrive65@yahoo.com>
2024-03-24 12:34:13 -0400

    Added Todo API Controller and Initial migration

---
+++ src/TodoApi/Controllers/TodoItemsController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Data;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TodoItemsController(TodoContext context) : ControllerBase
+{
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteTodoItem(long id)
+    {
+        var todoItem = await context.TodoItems.FindAsync(id);
+
+        if (todoItem is null)
+        {
+            return NotFound();
+        }
+
+        _ = context.TodoItems.Remove(todoItem);
+        _ = await context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+    {
+        var item = await context.TodoItems.FindAsync(id);
+
+        if (item is null)
+        {
+            return NotFound();
+        }
+
+        return item;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+    {
+        return await context.TodoItems.ToListAsync();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem item)
+    {
+        _ = context.TodoItems.Add(item);
+        _ = await context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetTodoItem), new { id = item.Id }, item);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutTodoItem(long id, TodoItem item)
+    {
+        if (id != item.Id)
+        {
+            return BadRequest();
+        }
+
+        context.Entry(item).State = EntityState.Modified;
+
+        try
+        {
+            _ = await context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException) when (!TodoItemExists(id))
+        {
+            return NotFound();
+        }
+
+        return NoContent();
+    }
+
+    private bool TodoItemExists(long id)
+    {
+        return context.TodoItems.Any(e => e.Id == id);
+    }
+
+    private static TodoItemDto ItemToDto(TodoItem todoItem) =>
+        new()
+        {
+            Id = todoItem.Id,
+            Name = todoItem.Name,
+            IsComplete = todoItem.IsComplete
+        };
+}
