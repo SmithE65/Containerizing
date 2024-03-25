a31aac7a82183e672729b7c12670651dccd136c2
Eric Smith <ydrive65@yahoo.com>
2024-03-24 21:46:02 -0400

    add docker-compose.yml

--- src/TodoApi/Program.cs
+++ src/TodoApi/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
 using TodoApi.Data;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
@@ -8,14 +9,23 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 // Add DbContext using SQL Server Provider
+var todoConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddDbContext<TodoContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(todoConnectionString));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 WebApplication app = builder.Build();
+
+// Create a logger instance
+var logger = app.Services.GetService<ILogger<Program>>()
+    ?? throw new InvalidOperationException("Failed to resolve logger for Program");
+
+// Log the connection string at the info level
+logger.LogInformation("Connection string: {ConnectionString}", todoConnectionString);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
